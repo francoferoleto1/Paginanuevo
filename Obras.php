ce81cc3f8882b6d34cee71793329d6ead7dbb049
francoferoleto1 <francofero68@gmail.com>
2018-08-30 15:26:55 -0300

    Pagina

---
+++ Obras.php
@@ -0,0 +1,52 @@
+<?php 
+
+  include'elementos.php';
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+</head>
+
+
+<body>
+
+<?php echo $menu ?>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 fondoferoleto">
+			</div>
+			<div class="col-md-6">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	
+				</p>
+			</div>
+		</div>
+	 </div>
+
+
+ 	
+
+
+
+<?php echo $final ?>
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+		
+</body>
+</html>
---
+++ Servicios.php
@@ -0,0 +1,55 @@
+<?php 
+
+  include'elementos.php';
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
+<body>
+
+<?php echo $menu ?>
+
+<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 fondoferoleto">
+			</div>
+			<div class="col-md-6">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	 </div>
+
+
+<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 fondoferoleto">
+			<img src="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA02/CONTENIDOS/201601/18/00107388500139____3__640x640.jpg">
+			</div>
+			<div class="col-md-6">
+				<p>Somos una empresa familiar que forjó sus bases sobre la responsabilidad, seriedad, compromiso, calidad y profesionalidad en cada uno de sus emprendimientos. El mercado evoca nuestro prestigio haciéndonos participe de los proyectos mas trascendentes. Crédito que respetamos con nuestra distinguida trayectoria. Gracias a ello crecimos, paulatinamente pero sobre seguro, nos mantuvimos firmes en un mercado siempre cambiante como el argentino. Supimos aceptar nuevos desafíos y atravesar las crisis, de las cuales salimos fortalecidos con mayor capacidad de desarrollo relevando competencia. 	
+				</p>
+			</div>
+		</div>
+	 </div>	
+
+<?php echo $final ?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</html>
