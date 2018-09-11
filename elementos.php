324bd994eaf3b89290803a4d62760160df7adc14
francoferoleto1 <francofero68@gmail.com>
2018-09-11 17:52:46 -0300

    all

--- elementos.php
+++ elementos.php
@@ -39,11 +39,11 @@
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
 
-          <!-- Content -->
-          <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
-          <p>Here you can use rows and columns here to organize your footer content.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate esse
-            quasi, veritatis totam voluptas nostrum.</p>
+         <!-- Content -->
+           <h5 class="font-weight-bold text-uppercase mb-4">Acerca de Nosotros</h5>
+           <p>Feroleto y Faga nació hace mas de 50 años como un emprendimiento familiar.</p>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate esse
+           quasi, veritatis totam voluptas nostrum.</p>
 
         </div>
         <!-- Grid column -->
@@ -59,7 +59,7 @@
           <ul class="list-unstyled">
             <li>
               <p>
-                <a href="#!">PROJECTS</a>
+                <a href="#!">Obras</a>
               </p>
             </li>
             <li>
@@ -88,24 +88,24 @@
         <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
 
           <!-- Contact details -->
-          <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+          <h5 class="font-weight-bold text-uppercase mb-4">Contacto</h5>
 
           <ul class="list-unstyled">
             <li>
               <p>
-                <i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+                <i class="fa fa-home mr-3"></i> Argentina, Buenos Aires.</p>
             </li>
             <li>
               <p>
-                <i class="fa fa-envelope mr-3"></i> info@example.com</p>
+                <i class="fa fa-envelope mr-3"></i> feroletofaga@feroleto.com</p>
             </li>
             <li>
               <p>
-                <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                <i class="fa fa-phone mr-3"></i> 4444-4444</p>
             </li>
             <li>
               <p>
-                <i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+                <i class="fa fa-print mr-3"></i> 9878-6545</p>
             </li>
           </ul>
 
@@ -113,32 +113,6 @@
         <!-- Grid column -->
 
         <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
-
-          <!-- Social buttons -->
-          <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
-
-          <!-- Facebook -->
-          <a type="button" class="btn-floating btn-fb">
-            <i class="fa fa-facebook"></i>
-          </a>
-          <!-- Twitter -->
-          <a type="button" class="btn-floating btn-tw">
-            <i class="fa fa-twitter"></i>
-          </a>
-          <!-- Google +-->
-          <a type="button" class="btn-floating btn-gplus">
-            <i class="fa fa-google-plus"></i>
-          </a>
-          <!-- Dribbble -->
-          <a type="button" class="btn-floating btn-dribbble">
-            <i class="fa fa-dribbble"></i>
-          </a>
-
-        </div>
-        <!-- Grid column -->
 
       </div>
       <!-- Grid row -->
@@ -148,14 +122,53 @@
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2018 Copyright:
-      <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
+      <a href="https://www.feroletofaga.com.ar/"> Feroletofaga.com</a>
     </div>
     <!-- Copyright -->
 
   </footer>
   <!-- Footer -->
 
+  ';
+
+    $fotos='
+ 
+  <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://wallpaper-gallery.net/images/sofia-carson-wallpaper/sofia-carson-wallpaper-5.jpg" alt="Los Angeles" width="900" height="300">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://wallpapersite.com/images/wallpapers/dove-cameron-3600x2025-hd-4k-11977.jpg" alt="Chicago" width="900" height="300">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://images3.alphacoders.com/775/thumb-1920-775680.jpg" alt="New York" width="900" height="300">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
   '
-  
 
  ?>
--- index.php
+++ index.php
@@ -12,7 +12,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
+  </style>
 </head>
 
 
@@ -20,7 +27,14 @@
 
 <?php echo $menu ?>
 
+
+	<?php echo $fotos ?>
+
   
+
+
+
+
 <div class="container-fluid">
 			<div class="col-md-6">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -45,7 +59,11 @@
 
 <?php echo $final ?>
   
-
+	
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
